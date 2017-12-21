e7fc9ba71c906af15e5061a7988ae6fa992eebe1
Dinis Rosario <dinis.rosario@miniclip.com>
2017-12-21 11:49:23 +0000

    Refactor request method

---
+++ src/erth_request.erl
@@ -0,0 +1,43 @@
+%%
+ % Miniclip SA, 2017. All rights reserved.
+ %
+-module(erth_request).
+-author("dinis.rosario").
+
+%% API
+-export([request/2]).
+
+-spec request(MethodData :: binary() | {binary(), integer()},
+              MethodArgs :: list()) -> {ok, any()} | {error, any()}.
+request(MethodName, MethodArgs) when is_binary(MethodName) ->
+    request({MethodName, undefined}, MethodArgs);
+request({MethodName, MethodId}, MethodArgs) when is_binary(MethodName) ->
+    Req = jsonrpc2_client:create_request({MethodName, MethodArgs, MethodId}),
+    Encoded = binary_to_list(jiffy:encode(Req)),
+    {ok, ConnPid} = gun:open("localhost", 8545),
+    {ok, _} = gun:await_up(ConnPid),
+    StreamRef = gun:post(ConnPid, "/",
+                         [{<<"content-type">>, <<"application/json">>}],
+                         Encoded),
+    case gun:await(ConnPid, StreamRef) of
+        {response, fin, _, _} ->
+            {error, failed};
+        {response, nofin, _, _} ->
+            {ok, Response} = gun:await_body(ConnPid, StreamRef),
+            case jiffy:decode(Response) of
+                {Decoded} ->
+                    process_decoded_body(Decoded);
+                _ ->
+                    {error, decoding_failed}
+            end
+    end.
+
+-spec process_decoded_body(DecodedBody :: list()) -> {ok, any()} | {error, any()}.
+process_decoded_body(DecodedBody) ->
+    case kvlist:get(<<"result">>, DecodedBody) of
+        undefined ->
+            Error = kvlist:get(<<"error">>, DecodedBody),
+            {error, Error};
+        Result ->
+            {ok, Result}
+    end.
--- src/erthereum.erl
+++ src/erthereum.erl
@@ -9,7 +9,8 @@
          eth_accounts/0,
          eth_sendTransaction/3]).
 %% personal namespace
--export([personal_newAccount/1]).
+-export([personal_newAccount/1,
+         personal_unlockAccount/2]).
 
 %% Type definitions.
 -type error() :: failed | decoding_failed.
@@ -17,7 +18,9 @@
                                  eth_blockNumber |
                                  eth_getBalance |
                                  eth_accounts |
-                                 personal_newAccount.
+                                 eth_sendTransaction |
+                                 personal_newAccount |
+                                 personal_unlockAccount.
 -type method_name() :: binary().
 -type method_id() :: non_neg_integer().
 -type address() :: binary().
@@ -38,12 +41,12 @@
 %% Returns the current network protocol version.
 -spec net_version() -> {ok, non_neg_integer()} | {error, error()}.
 net_version() ->
-    maybe_int(request(management_api_data(net_version))).
+    maybe_int(request(net_version)).
 
 %% Returns the number of most recent block.
 -spec eth_blockNumber() -> {ok, non_neg_integer()} | {error, error()}.
 eth_blockNumber() ->
-    maybe_int(request(management_api_data(eth_blockNumber))).
+    maybe_int(request(eth_blockNumber)).
 
 %% Returns the balance of the account of given address.
 -spec eth_getBalance(Address :: address()) -> {ok, non_neg_integer()} | {error, error()}.
@@ -56,13 +59,12 @@ eth_getBalance(Address) ->
         -> {ok, non_neg_integer()} | {error, error()}.
 eth_getBalance(Address, BlockNumberOrTag0) ->
     BlockNumberOrTag = block_number_or_tag(BlockNumberOrTag0),
-    maybe_int(request(management_api_data(eth_getBalance),
-              [Address, BlockNumberOrTag])).
+    maybe_int(request(eth_getBalance, [Address, BlockNumberOrTag])).
 
 %% Returns a list of addresses owned by client.
 -spec eth_accounts() -> {ok, list(address())} | {error, error()}.
 eth_accounts() ->
-    maybe_list(request(management_api_data(eth_accounts))).
+    maybe_list(request(eth_accounts)).
 
 %% Creates new message call transaction or a contract creation, if the data field contains code
 -spec eth_sendTransaction(FromAddress :: address(),
@@ -72,7 +74,7 @@ eth_sendTransaction(FromAddress, ToAddress, Value) ->
     Params = [{[{<<"from">>, FromAddress},
               {<<"to">>, ToAddress},
               {<<"value">>, eth_int(Value)}]}],
-    maybe_binary(request(management_api_data(eth_sendTransaction), Params)).
+    maybe_binary(request(eth_sendTransaction, Params)).
 
 
 %% Generates a new private key and stores it in the key storese directory.
@@ -81,65 +83,52 @@ eth_sendTransaction(FromAddress, ToAddress, Value) ->
 -spec personal_newAccount(Passphrase :: passphrase())
         -> {ok, address()} | {error, error()}.
 personal_newAccount(Passphrase) ->
-    maybe_binary(request(management_api_data(personal_newAccount),
-                         [Passphrase])).
+    maybe_binary(request(personal_newAccount, [Passphrase])).
+
+-spec personal_unlockAccount(Address :: address(),
+                             Passphrase :: passphrase()
+                            )
+                            -> {ok, address()} | {error, error()}.
+personal_unlockAccount(Address, Passphrase) ->
+    maybe_binary(request(personal_unlockAccount, [Address, Passphrase])).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+-spec request(Method :: management_api_method()) -> any() | {error, any()}.
+request(Method) ->
+    request(Method, []).
 
-request(MethodData) ->
-    request(MethodData, []).
-
-request(MethodName, MethodArgs) when is_binary(MethodName) ->
-    request({MethodName, undefined}, MethodArgs);
-request({MethodName, MethodId}, MethodArgs) when is_binary(MethodName) ->
-    Req = jsonrpc2_client:create_request({MethodName, MethodArgs, MethodId}),
-    Encoded = binary_to_list(jiffy:encode(Req)),
-    {ok, ConnPid} = gun:open("localhost", 8545),
-    {ok, _} = gun:await_up(ConnPid),
-    StreamRef = gun:post(ConnPid, "/",
-                         [{<<"content-type">>, <<"application/json">>}],
-                         Encoded),
-    case gun:await(ConnPid, StreamRef) of
-        {response, fin, _, _} ->
-            {error, failed};
-        {response, nofin, _, _} ->
-            {ok, Response} = gun:await_body(ConnPid, StreamRef),
-            case jiffy:decode(Response) of
-                {Decoded} ->
-                    process_decoded_body(Decoded);
-                _ ->
-                    {error, decoding_failed}
-            end
-    end.
+-spec request(Method :: management_api_method(),
+              Arguments :: list()) -> any() | {error, any()}.
+request(Method, Arguments) ->
+    erth_request:request(management_api_data(Method), Arguments).
 
 -spec block_number_or_tag(BlockNumberOrTag :: non_neg_integer() | block_tag()) -> binary().
 block_number_or_tag(latest) -> <<"latest">>;
 block_number_or_tag(earliest) -> <<"earliest">>;
 block_number_or_tag(pending) -> <<"pending">>;
-block_number_or_tag(BlockNumber) when is_integer(BlockNumber) ->
-    list_to_binary("0x" ++ integer_to_list(BlockNumber, 16)).
+block_number_or_tag(BlockNumber) when is_integer(BlockNumber) -> eth_int(BlockNumber).
 
--spec maybe_int(binary() | error()) -> {ok, integer()} | error().
+-spec maybe_int({ok, binary()} | error()) -> {ok, integer()} | error().
 maybe_int({error, _} = Error) -> Error;
-maybe_int(<<"0x", Bin/binary>>) ->
-    maybe_int(Bin);
-maybe_int(Bin) ->
+maybe_int({ok, <<"0x", Bin/binary>>}) ->
+    maybe_int({ok, Bin});
+maybe_int({ok, Bin}) ->
     case catch binary_to_integer(Bin) of
         I when is_integer(I) -> {ok, I};
         _ ->
             {ok, binary_to_integer(Bin, 16)}
     end.
 
--spec maybe_binary(binary() | error()) -> {ok, binary()} | error().
+-spec maybe_binary({ok, binary()} | error()) -> {ok, binary()} | error().
 maybe_binary({error, _} = Error) -> Error;
-maybe_binary(Bin) when is_binary(Bin) ->
+maybe_binary({ok, Bin}) when is_binary(Bin) ->
     {ok, Bin}.
 
--spec maybe_list(list(binary()) | error()) -> {ok, list(binary())} | error().
+-spec maybe_list({ok, list(binary())} | error()) -> {ok, list(binary())} | error().
 maybe_list({error, _} = Error) -> Error;
-maybe_list(L) when is_list(L) ->
+maybe_list({ok, L}) when is_list(L) ->
     {ok, L}.
 
 -spec eth_int(Integer :: integer()) -> binary().
@@ -156,17 +145,12 @@ management_api_data(eth_getBalance) ->
     {<<"eth_getBalance">>, 1};
 management_api_data(personal_newAccount) ->
     <<"personal_newAccount">>;
+management_api_data(personal_unlockAccount) ->
+    <<"personal_unlockAccount">>;
 management_api_data(eth_accounts) ->
     {<<"eth_accounts">>, 1};
 management_api_data(eth_sendTransaction) ->
     {<<"eth_sendTransaction">>, 1}.
 
-process_decoded_body(DecodedBody) ->
-    case kvlist:get(<<"result">>, DecodedBody) of
-        undefined ->
-            Error = kvlist:get(<<"error">>, DecodedBody),
-            {error, Error};
-        Result when is_binary(Result) ->
-            Result
-    end.
+
 
