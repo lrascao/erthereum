39e76db9770ee0a752386528458b45be7b7a2ddd
Dinis Rosario <dinis.rosario@miniclip.com>
2017-12-21 11:02:54 +0000

    Add eth_sendTransaction method

--- src/erthereum.erl
+++ src/erthereum.erl
@@ -6,7 +6,8 @@
 %% eth namespace
 -export([eth_blockNumber/0,
          eth_getBalance/1, eth_getBalance/2,
-         eth_accounts/0]).
+         eth_accounts/0,
+         eth_sendTransaction/3]).
 %% personal namespace
 -export([personal_newAccount/1]).
 
@@ -22,6 +23,8 @@
 -type address() :: binary().
 -type passphrase() :: binary().
 -type block_tag() :: latest | earliest | pending.
+-type quantity() :: integer().
+-type data() :: binary().
 
 %% Type exports
 -export_type([error/0]).
@@ -61,6 +64,17 @@ eth_getBalance(Address, BlockNumberOrTag0) ->
 eth_accounts() ->
     maybe_list(request(management_api_data(eth_accounts))).
 
+%% Creates new message call transaction or a contract creation, if the data field contains code
+-spec eth_sendTransaction(FromAddress :: address(),
+                          ToAddress :: address(),
+                          Value :: quantity()) -> {ok, TransactionHash :: data()} | {error, error()}.
+eth_sendTransaction(FromAddress, ToAddress, Value) ->
+    Params = [{[{<<"from">>, FromAddress},
+              {<<"to">>, ToAddress},
+              {<<"value">>, eth_int(Value)}]}],
+    maybe_binary(request(management_api_data(eth_sendTransaction), Params)).
+
+
 %% Generates a new private key and stores it in the key storese directory.
 %% The key file is encrypted with the given passphrase.
 %% Returns the address of the new account.
@@ -94,8 +108,9 @@ request({MethodName, MethodId}, MethodArgs) when is_binary(MethodName) ->
             {ok, Response} = gun:await_body(ConnPid, StreamRef),
             case jiffy:decode(Response) of
                 {Decoded} ->
-                    proplists:get_value(<<"result">>, Decoded);
-                _ -> {error, decoding_failed}
+                    process_decoded_body(Decoded);
+                _ ->
+                    {error, decoding_failed}
             end
     end.
 
@@ -127,6 +142,10 @@ maybe_list({error, _} = Error) -> Error;
 maybe_list(L) when is_list(L) ->
     {ok, L}.
 
+-spec eth_int(Integer :: integer()) -> binary().
+eth_int(Integer) when is_integer(Integer) ->
+    <<"0x", (integer_to_binary(Integer, 16))/binary>>.
+
 -spec management_api_data(Method :: management_api_method())
         -> {method_name(), method_id()} | method_name().
 management_api_data(net_version) ->
@@ -138,5 +157,16 @@ management_api_data(eth_getBalance) ->
 management_api_data(personal_newAccount) ->
     <<"personal_newAccount">>;
 management_api_data(eth_accounts) ->
-    {<<"eth_accounts">>, 1}.
+    {<<"eth_accounts">>, 1};
+management_api_data(eth_sendTransaction) ->
+    {<<"eth_sendTransaction">>, 1}.
+
+process_decoded_body(DecodedBody) ->
+    case kvlist:get(<<"result">>, DecodedBody) of
+        undefined ->
+            Error = kvlist:get(<<"error">>, DecodedBody),
+            {error, Error};
+        Result when is_binary(Result) ->
+            Result
+    end.
 
